0b56343f95d4bffc6fdf78b12647a2ad039a490e
kingribeiro21-afk <kingribeiro21@gmail.com>
2026-08-16 10:07:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "APAKAH KAMU BENER BENER CINTA? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
